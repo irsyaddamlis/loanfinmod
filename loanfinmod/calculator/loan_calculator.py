31229718723ec79ccdeda10fc0f9e22cfb959f90
Irsyad Damlis <irsyad.damlis@gmail.com>
2026-05-30 12:04:36 +0700

    erase np output

--- loanfinmod/calculator/loan_calculator.py
+++ loanfinmod/calculator/loan_calculator.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class LoanCalculator:
     @staticmethod
     def pmt(ntf, annual_rate_pct, tenure) -> float:
@@ -7,31 +8,28 @@ class LoanCalculator:
             return 0.0
         r = (annual_rate_pct / 100) / 12
         if r == 0:
-            return ntf / tenure
-        return (ntf * r / (1 - (1 + r) ** -tenure))
+            return float(ntf / tenure)
+        return float(ntf * r / (1 - (1 + r) ** -tenure))
 
     @staticmethod
     def installment_amount(ntf, annual_rate_pct, tenure) -> float:
         if tenure <= 0:
             return 0.0
         r = (annual_rate_pct / 100) / 12
-        pmt = 0
         if r == 0:
-            pmt = ((ntf / tenure) / 100) * 100
-            return pmt
-        
-        pmt = (ntf * r / (1 - (1 + r) ** -tenure)) 
-        return np.round(pmt/100, 0) * 100
+            return float((ntf / tenure) / 100 * 100)
+        pmt = ntf * r / (1 - (1 + r) ** -tenure)
+        return float(round(pmt / 100) * 100)
 
     @staticmethod
     def _principal_payment(pmt, osp_last_month, annual_rate_pct):
         monthly_rate = (annual_rate_pct / 100) / 12
-        return pmt - (osp_last_month * monthly_rate)
+        return float(pmt - (osp_last_month * monthly_rate))
 
     @staticmethod
     def _osp_current(osp_last_month, principal_payment):
-        return osp_last_month - principal_payment
+        return float(osp_last_month - principal_payment)
 
     @staticmethod
     def _interest_income(installment, principal_payment):
-        return installment - principal_payment
+        return float(installment - principal_payment)
